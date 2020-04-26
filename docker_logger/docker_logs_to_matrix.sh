efc3e460d240f3afda650587b71e64a3e04da059
Joey Stevens <joey.stevens00@gmail.com>
2020-04-26 10:02:21 -0500

    use env var to pass the password

--- docker_logger/docker_logs_to_matrix.sh
+++ docker_logger/docker_logs_to_matrix.sh
@@ -1,8 +1,10 @@
 while read LINE; do export "$LINE"; done < /app/env
 
+export MATRIX_PASSWORD="$password"
+
 docker container ls\
   | rev | awk '{print $1}' | rev\
   | grep -vE '^NAMES$'\
   | grep -vi synapse\
   | xargs -I{} bash -c \
-    'docker container logs --since 1m {} | python3 /app/src/log.py --host "$host" --user "$user" --password "$password" --room docker_logs_{}'
+    'docker container logs --since 1m {} | python3 /app/src/log.py --host "$host" --user "$user" --room docker_logs_{}'
