e75ae0255708a90d641fc0dae8ac31d28dc80cd7
Joey Stevens <joey.stevens00@gmail.com>
2020-04-27 16:39:09 -0500

    env vars name change

--- docker_logger/scripts/docker_logs_to_matrix.sh
+++ docker_logger/scripts/docker_logs_to_matrix.sh
@@ -1,10 +1,13 @@
 while read LINE; do export "$LINE"; done < /app/env
 
-export MATRIX_PASSWORD="$password"
-
-docker container ls\
-  | rev | awk '{print $1}' | rev\
-  | grep -vE '^NAMES$'\
-  | grep -vi synapse\
-  | xargs -I{} bash -c \
-    'docker container logs --since 5m {} | python3 /app/src/log.py --host "$host" --user "$user" --room docker_logs_{}'
+for container in $(\
+  docker container ls\
+    | rev | awk '{print $1}' | rev\
+    | grep -vE '^NAMES$'\
+    | grep -vi synapse\
+    | xargs -I{} bash -c \
+)
+do
+  docker container logs --since 5m $container\
+    | python3 /app/src/log.py --host "$MATRIX_HOST" --user "$MATRIX_USER" --room docker_logs_$container
+done
--- docker_logger/scripts/log_stats.sh
+++ docker_logger/scripts/log_stats.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 while read LINE; do export "$LINE"; done < /app/env
-export MATRIX_PASSWORD="$password"
 
 bash /app/docker_logger/scripts/stats.sh\
-  | python3 /app/src/log.py --host "$host" --user "$user" --room server_stats
+  | python3 /app/src/log.py --host "$MATRIX_HOST" --user "$MATRIX_USER" --room server_stats
