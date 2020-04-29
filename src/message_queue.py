0433b9d9071bba849d0137e2e3897e98a1fe9c65
Joey Stevens <joey.stevens00@gmail.com>
2020-04-28 20:08:27 -0500

    attempt to deliver messages at a steady rate when injestion is batched

--- src/message_queue.py
+++ src/message_queue.py
@@ -31,6 +31,7 @@ class Settings(BaseSettings):
     matrix_user: str
     matrix_host: str
     matrix_password: str
+    delivery_interval: int = 5 # in minutes
 
 
 class MessageInjest(BaseModel):
@@ -54,6 +55,40 @@ async def sleep_time():
     asyncio.sleep(random.choice(range(1000, 5000))/1000)
 
 
+async def sleep_weighted(r, minutes=5):
+    """Sleep weighted by the client delivery interval and queue size."""
+    count = r.llen('injest')
+    seconds = minutes*60
+    if count:
+        w=seconds/count
+        logger.debug(f" Sleeping for {w} ({count} messages in the queue, client push interval assumed to be every {minutes} minutes)")
+        asyncio.sleep(w)
+
+
+async def send_message(r, m):
+    logger.debug("Dequeue")
+    settings = Settings()
+    await sleep_weighted(r, minutes=settings.delivery_interval)
+    d = json.loads(m.decode('utf-8'))
+    logger.debug(f"Processing message: {d}")
+    if d['attempts'] >= d['max_attempts']:
+        logger.warning("Max tries hit for message.")
+        logger.debug(f"Message content: {d}")
+        await sleep_time()
+        return (await dequeue())
+    ret = await send_matrix_message(
+        Message(
+            **d['message'],
+            host=settings.matrix_host,
+            password=settings.matrix_password,
+            user=settings.matrix_user,
+        ),
+        format_func=lambda s: s,
+    )
+    logger.debug(f"Sent message and got back: {ret}")
+    return ret
+
+
 async def dequeue_messages():
     async def dequeue():
         r = redis_handle()
@@ -62,25 +97,7 @@ async def dequeue_messages():
             await sleep_time()
             return (await dequeue())
         try:
-            logger.debug("Dequeue")
-            settings = Settings()
-            d = json.loads(m.decode('utf-8'))
-            logger.debug(f"Processing message: {d}")
-            if d['attempts'] >= d['max_attempts']:
-                logger.warning("Max tries hit for message.")
-                logger.debug(f"Message content: {d}")
-                await sleep_time()
-                return (await dequeue())
-            ret = await send_matrix_message(
-                Message(
-                    **d['message'],
-                    host=settings.matrix_host,
-                    password=settings.matrix_password,
-                    user=settings.matrix_user,
-                ),
-                format_func=lambda s: s,
-            )
-            logger.debug(f"Sent message and got back: {ret}")
+            await send_message(r, m)
         except Exception as e:
             logger.warning(f"Caught an error while sending message: {e}")
             if type(e) == pydantic.error_wrappers.ValidationError:
